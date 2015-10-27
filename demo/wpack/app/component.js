777e626c67a9e65313fd4b785b03432f5076928b
Tanuj Dave <tanuj.dave@mediatech.co.in>
2015-10-27 16:30:01 +0530

    add wpack

---
+++ demo/wpack/app/component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function () {
+    var element = document.createElement('h1');
+
+    element.innerHTML = 'React world 2015-16';
+
+    return element;
+};
---
+++ demo/wpack/app/main.js
@@ -0,0 +1,5 @@
+'use strict'
+var component = require('./component.js');
+var message = require('./message.js');
+
+document.body.appendChild(component());
